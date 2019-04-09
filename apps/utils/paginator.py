1c10d0d444fab3aab4c40e0f822040bd7f72b019
cabbyw <wangsy@condata.cn>
2019-04-09 12:46:47 +0800

    overwrite paginator

--- apps/utils/paginator.py
+++ apps/utils/paginator.py
@@ -5,7 +5,7 @@ Create by 王思勇 on 2019/2/12
 """
 from collections import OrderedDict
 
-from rest_framework.pagination import PageNumberPagination
+from rest_framework.pagination import PageNumberPagination, _positive_int
 from rest_framework.response import Response
 
 from setting.models import Setting
@@ -16,9 +16,9 @@ class CustomPagination(PageNumberPagination):
     自定义分页类， 让分页可以配置
     """
 
-    # page_size = 30
-    # page_size_query_param = 'page_size'
-    # max_page_size = 10000
+    page_size = 30
+    page_size_query_param = 'page_size'
+    max_page_size = 1000
 
     def get_paginated_response(self, data):
         """
@@ -35,12 +35,26 @@ class CustomPagination(PageNumberPagination):
     def get_page_size(self, request):
         """
         配置每页显示行数
+        page_size使用顺序
+        1. 查询参数中指定page_size
+        2. 配置中的page_size
+        3. 默认
         """
-        # TODO 每次都需要去查询数据库， 是否可以通过刷新django配置， 或信号的方式来解决?
-        try:
-            setting = Setting.objects.get(option='pagination')
-            page_size = int(setting.value)
-        except (Setting.DoesNotExist, ValueError):
-            page_size = 30
-        self.page_size = page_size
-        return page_size
+        if self.page_size_query_param:
+            try:
+                self.page_size = _positive_int(
+                    request.query_params[self.page_size_query_param],
+                    strict=True,
+                    cutoff=self.max_page_size
+                )
+            except (KeyError, ValueError):
+                # 使用配置
+                # TODO 每次都需要去查询数据库， 是否可以通过刷新django配置，
+                #  或信号的方式来解决?
+                try:
+                    setting = Setting.objects.get(option='pagination')
+                    self.page_size = int(setting.value)
+                except (Setting.DoesNotExist, ValueError):
+                    self.page_size = 30
+
+        return self.page_size
