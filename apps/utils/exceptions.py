7a1a4606a45d316989559bee8440b66ea294a9a2
cabbyw <wangsy@condata.cn>
2019-04-16 11:55:47 +0800

    custom exceptions inherit from APIException

--- apps/utils/exceptions.py
+++ apps/utils/exceptions.py
@@ -13,3 +13,15 @@ class DeleteOnlineHubError(APIException):
     status_code = status.HTTP_400_BAD_REQUEST
     default_detail = _("can't delete the hub online")
     default_code = 'invalid'
+
+
+class ConnectNSError(APIException):
+    status_code = status.HTTP_408_REQUEST_TIMEOUT
+    default_detail = _("can't connect network service")
+    default_code = 'server error'
+
+
+class AuthenticateNSError(APIException):
+    status_code = status.HTTP_408_REQUEST_TIMEOUT
+    default_detail = _("authenticate failed when connect network service")
+    default_code = 'authentication error'
